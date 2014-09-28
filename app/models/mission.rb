de688ef1ca6387792a21d7d68d4c6227f66c17d1
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-09-28 20:21:23 +0900

    after_initializeの試行

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -6,4 +6,19 @@ class Mission < ActiveRecord::Base
   has_many :assigns
 
   accepts_nested_attributes_for :acquisitions
+
+=begin
+  after_initialize :add_acquisitions
+
+  private
+
+    def add_acquisitions
+      if new_record?
+        categories = Category.all
+        categories.each do |category|
+          self.acquisitions.build(category_id: category.id, experience: 0)
+        end
+      end
+    end
+=end
 end
