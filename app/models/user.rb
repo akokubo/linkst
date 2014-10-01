f096f609f5c30fd11718f57f20b2b62a782e21ed
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-10-01 14:12:29 +0900

    ミッションの割当関数を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,15 +24,44 @@ class User < ActiveRecord::Base
     Level.get_level_from_experience(average_experience)
   end
 
+  def tone(level, random)
+    level_value = level.value
+    level_value = (level_value - 1.0) / 8.0
+
+    x = random.rand(1.0)
+    a = 4.0 / level_value
+    z = 1.0
+    value = (-Math.log(1.0 / x - 1.0) / a + level_value) / z
+    if value > 1.0
+      value = 1.0
+    elsif value < 0.0
+      value = 0.0
+    end
+
+    tone_level_value = value
+    tone_level_value = (tone_level_value * 8 + 1).to_i
+
+    tone_level = Level.find_by(value: tone_level_value)
+    tone_level
+  end
+
   def reassign_missions
-    assigns = self.assigns
-    missions = Mission.all
-    missions_count = missions.count
+    self.assigns.destroy_all
+
+    categories = Category.all
+
     random = Random.new
-    assigns.each do |assign|
+
+    categories.each do |category|
+      status = self.statuses.find_by(category_id: category.id)
+      level = Level.get_level_from_experience(status.experience)
+      mission_level = tone(level, random)
+      missions = Mission.where(category_id: category.id, level_id: mission_level.id)
+
+      missions_count = missions.count
       index = random.rand(missions_count)
-      assign.mission_id = missions[index].id
-      assign.save
+      mission = missions[index]
+      self.assigns.create(mission_id: mission.id)
     end
   end
 
