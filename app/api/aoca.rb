f89cb7059e112a6136a5a2bbca29956a37408253
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-10-06 08:44:07 +0900

    APIの返す内容をわかりやすく

--- app/api/aoca.rb
+++ app/api/aoca.rb
@@ -12,7 +12,17 @@ module Aoca
     resource :users do
       desc "Users ranking."
       get '/' do
-        User.limit(10).order('created_at DESC')
+        users = User.limit(10).order('created_at DESC')
+        hashes = []
+        users.each do |user|
+          hash = {
+            name: user.name,
+            total_experience: user.total_experience,
+            average_level: user.average_level.value
+          }
+          hashes << hash
+        end
+        hashes
       end
 
       desc "Return a status."
@@ -21,7 +31,42 @@ module Aoca
       end
       route_param :idm do
         get do
-          User.find_by(idm: params[:idm])
+          user = User.find_by(idm: params[:idm])
+          categories = Category.all
+          hash = {
+            number: user.number,
+            role: user.role.japanese_name,
+            name: user.name,
+            email: user.email,
+            idm: user.idm,
+            average_level: user.average_level.value,
+            total_experience: user.total_experience,
+            next_average_level_required_experience: user.average_level.next.required_experience,
+            statuses: [
+            ],
+            missions: [
+            ]
+          }
+          categories.each do |category|
+            status = user.statuses.find_by(category_id: category.id)
+            level = Level.get_level_from_experience(status.experience)
+            hash[:statuses] << {
+              name: category.name,
+              experience: status.experience,
+              level: level.value,
+              next_level_required_experience: level.next.required_experience
+            }
+          end
+          user.assigns.each do |assign|
+            mission = assign.mission
+            hash[:missions] << {
+              id: mission.id,
+              name: mission.category.name,
+              level: mission.level.value,
+              description: mission.description
+            }
+          end
+          hash
         end
       end
 
@@ -30,7 +75,18 @@ module Aoca
     resource :histories do
       desc "Return a public timeline."
       get '/' do
-        History.limit(10).order('created_at DESC')
+        histories = History.limit(10).order('created_at DESC')
+        hashes = []
+        histories.each do |history|
+          hashes << {
+            user: history.user.name,
+            category: history.mission.category.name,
+            level: history.mission.level.value,
+            mission: history.mission.description,
+            created_at: history.created_at
+          }
+        end
+        hashes
       end
 
       desc "Create a history."
@@ -56,6 +112,27 @@ module Aoca
           user.reassign_missions
           history.save
           history
+        end
+      end
+
+      desc "Return histories."
+      params do
+        requires :idm, type: String, desc: "Your idm."
+      end
+      route_param :idm do
+        get do
+          user = User.find_by(idm: params[:idm])
+          histories = user.histories
+          hashes = []
+          histories.each do |history|
+            hashes << {
+              category: history.mission.category.name,
+              level: history.mission.level.value,
+              mission: history.mission.description,
+              created_at: history.created_at
+            }
+          end
+          hashes
         end
       end
     end
