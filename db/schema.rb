06d495b0baddf831ad70a8a0ada1cf0facc50e31
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-10-30 00:03:59 +0900

    ミッションの修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027182238) do
+ActiveRecord::Schema.define(version: 20141029121409) do
 
   create_table "acquisitions", force: true do |t|
     t.integer  "mission_id"
@@ -95,12 +95,12 @@ ActiveRecord::Schema.define(version: 20141027182238) do
   add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20141027182238) do
     t.integer  "role_id"
     t.integer  "seminar_id"
     t.string   "card_number"
+    t.date     "recent_web_access_bonus_awarded_on"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
